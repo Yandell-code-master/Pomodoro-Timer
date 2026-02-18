1cda40583ecc95576e6968e7b0b6f147183dbb06
Yandell-code-master <152900887+Yandell-code-master@users.noreply.github.com>
2026-02-17 19:35:49 -0600

    I created the Dockerfile and the docker-compose so the app is in production state, whith this i can send the app to everyone and they can use it whiout problem.  1. I create the Dockerfile that creates the image that has my java app 2. I create the docker-compose that has all the configuration of the creation of the container of my app and the db container 3. I modify something without matter in the sing-up.js

--- js/mail-sing-up.js
+++ js/mail-sing-up.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", event => {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
 
     const form = document.getElementById("email-form");
     const mailInput = document.getElementById("email-input");
 
-    form.addEventListener("submit", () => {
+    form.addEventListener("submit", async event => {
         event.preventDefault();
         const URL = "http://localhost:8080/users/save-user-email";
         const email = mailInput.value;
-        mailInput.value = "";
 
         const userFromEmail = {
             name: "newUser",
@@ -17,7 +15,7 @@ document.addEventListener("DOMContentLoaded", event => {
             status: "PENDING"
         }
 
-        fetch(URL, {
+        const response = await fetch(URL, {
             method: 'POST',
             body: JSON.stringify(userFromEmail),
             headers: {
@@ -25,6 +23,11 @@ document.addEventListener("DOMContentLoaded", event => {
             }
         });
 
+        if (!response.ok) {
+            throw new Error("Something went wrong trying to sing up");
+        }
+
+        mailInput.value = "";
         window.location.href = "http://localhost:8000/activate-page.html";
     });
 });
